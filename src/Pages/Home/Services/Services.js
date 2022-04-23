d47e3d69e0e8e42691b9236d609d31d3fc0f5e48
tanjid111 <tanjidhossain@gmail.com>
2022-04-23 15:24:02 -0700

    client app updated

---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import Service from '../Service/Service';
+import './Services.css'
+
+const Services = () => {
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/service')
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, [])
+
+    return (
+        <div id='services' className='container'>
+            <div className="row">
+                <h2 className='text-primary text-center my-5'>Our Services:</h2>
+                <div className='services-container'>
+                    {
+                        services.map(service => <Service
+                            key={service._id}
+                            service={service}
+                        ></Service>)
+                    }
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
